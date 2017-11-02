442045462bf7cfc785dfd4a6734cc55170abed1a
hlWang <whl413035073@qq.com>
2017-11-02 22:12:51 +0800

    debug

--- main.py
+++ main.py
@@ -20,78 +20,78 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-def parse_args():
-	'''
-	Parses the node2vec arguments.
-	'''
-	parser = argparse.ArgumentParser(description="Run node2vec.")
-
-	parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
-	                    help='Input graph path')
-
-	parser.add_argument('--output', nargs='?', default='emb/karate.emb',
-	                    help='Embeddings path')
-
-	parser.add_argument('--dimensions', type=int, default=128,
-	                    help='Number of dimensions. Default is 128.')
-
-	parser.add_argument('--walk-length', type=int, default=80,
-	                    help='Length of walk per source. Default is 80.')
-
-	parser.add_argument('--num-walks', type=int, default=10,
-	                    help='Number of walks per source. Default is 10.')
-
-	parser.add_argument('--window-size', type=int, default=10,
-                    	help='Context size for optimization. Default is 10.')
-
-	parser.add_argument('--iter', default=1, type=int,
-                      help='Number of epochs in SGD')
-
-	parser.add_argument('--workers', type=int, default=8,
-	                    help='Number of parallel workers. Default is 8.')
-
-	parser.add_argument('--p', type=float, default=1,
-	                    help='Return hyperparameter. Default is 1.')
-
-	parser.add_argument('--q', type=float, default=1,
-	                    help='Inout hyperparameter. Default is 1.')
-
-	parser.add_argument('--weighted', dest='weighted', action='store_true',
-	                    help='Boolean specifying (un)weighted. Default is unweighted.')
-	parser.add_argument('--unweighted', dest='unweighted', action='store_false')
-	parser.set_defaults(weighted=False)
-
-	parser.add_argument('--directed', dest='directed', action='store_true',
-	                    help='Graph is (un)directed. Default is undirected.')
-	parser.add_argument('--undirected', dest='undirected', action='store_false')
-	parser.set_defaults(directed=False)
-
-	return parser.parse_args()
+# def parse_args():
+#     '''
+#     Parses the node2vec arguments.
+#     '''
+#     parser = argparse.ArgumentParser(description="Run node2vec.")
+# 
+#     parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
+#                         help='Input graph path')
+# 
+#     parser.add_argument('--output', nargs='?', default='emb/karate.emb',
+#                         help='Embeddings path')
+# 
+#     parser.add_argument('--dimensions', type=int, default=128,
+#                         help='Number of dimensions. Default is 128.')
+# 
+#     parser.add_argument('--walk-length', type=int, default=80,
+#                         help='Length of walk per source. Default is 80.')
+# 
+#     parser.add_argument('--num-walks', type=int, default=10,
+#                         help='Number of walks per source. Default is 10.')
+# 
+#     parser.add_argument('--window-size', type=int, default=10,
+#                         help='Context size for optimization. Default is 10.')
+# 
+#     parser.add_argument('--iter', default=1, type=int,
+#                       help='Number of epochs in SGD')
+# 
+#     parser.add_argument('--workers', type=int, default=8,
+#                         help='Number of parallel workers. Default is 8.')
+# 
+#     parser.add_argument('--p', type=float, default=1,
+#                         help='Return hyperparameter. Default is 1.')
+# 
+#     parser.add_argument('--q', type=float, default=1,
+#                         help='Inout hyperparameter. Default is 1.')
+# 
+#     parser.add_argument('--weighted', dest='weighted', action='store_true',
+#                         help='Boolean specifying (un)weighted. Default is unweighted.')
+#     parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+#     parser.set_defaults(weighted=False)
+# 
+#     parser.add_argument('--directed', dest='directed', action='store_true',
+#                         help='Graph is (un)directed. Default is undirected.')
+#     parser.add_argument('--undirected', dest='undirected', action='store_false')
+#     parser.set_defaults(directed=False)
+# 
+#     return parser.parse_args()
 
 def read_graph():
-	'''
-	Reads the input network in networkx.
-	'''
-	if args.weighted:
-		G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
-	else:
-		G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
-		for edge in G.edges():
-			G[edge[0]][edge[1]]['weight'] = 1
+    '''
+    Reads the input network in networkx.
+    '''
+    if args.weighted:
+        G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
+    else:
+        G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
+        for edge in G.edges():
+            G[edge[0]][edge[1]]['weight'] = 1
 
-	if not args.directed:
-		G = G.to_undirected()
+    if not args.directed:
+        G = G.to_undirected()
 
-	return G
+    return G
 
 def learn_embeddings(walks):
-	'''
-	Learn embeddings by optimizing the Skipgram objective using SGD.
-	'''
-	walks = [map(str, walk) for walk in walks]
-	model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
-	model.save_word2vec_format(args.output)
-	return
+    '''
+    Learn embeddings by optimizing the Skipgram objective using SGD.
+    '''
+    walks = [map(str, walk) for walk in walks]
+    model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
+    model.save_word2vec_format(args.output)
+    return
 
 class Node2Vec(nn.Module):
     def __init__(self, NODE_SIZE, CONTEXT_SIZE, EMBEDDING_SIZE):
@@ -99,56 +99,82 @@ class Node2Vec(nn.Module):
         self.embeddings = nn.Embedding(NODE_SIZE, EMBEDDING_SIZE)
         self.linear1 = nn.Linear(CONTEXT_SIZE * EMBEDDING_SIZE, 128)
         self.linear2 = nn.Linear(128, NODE_SIZE)
-		self.classif1 = nn.Linear(EMBEDDING_SIZE, 64)
-		self.classif2 = nn.Linear(64, 1)
+        self.classif1 = nn.Linear(EMBEDDING_SIZE, 64)
+        self.classif2 = nn.Linear(64, 1)
 
     def forward(self, inputs):
         embeds = self.embeddings(inputs[:-1]).view((1, -1))
-		clas_embed = self.embeddings(inputs[-1]).view((1,-1))
+        clas_embed = self.embeddings(inputs[-1]).view((1,-1))
         out = F.relu(self.linear1(embeds))
         out = self.linear2(out)
         log_probs = F.log_softmax(out)
-		out = F.relu(self.classif1(clas_embed))
-		out = self.classif2(out)
-		clas_log_probs = F.log_softmax(out)
+        out = F.relu(self.classif1(clas_embed))
+        out = self.classif2(out)
+        clas_log_probs = F.log_softmax(out)
         return log_probs, clas_log_probs
 
 
 
 
 
-def train_node2vec(EMBEDDING_SIZE, CONTEXT_SIZE, NODE_SIZE, Random_path):
-	losses = []
-	loss_function = nn.NLLLoss()
-	model = Node2Vec(NODE_SIZE, CONTEXT_SIZE, EMBEDDING_SIZE)
-	optimizer = optim.SGD(model.parameters(), lr=0.001)
-	for epoch in range(10):
-		total_loss = torch.Tensor([0])
-		for context, target in Random_path:
-			context_var = autograd.Variable(torch.LongTensor(context))
-			model.zero_grad()
-			log_probs, clas_log_probs = model(context_var)
-			loss1 = loss_function(log_probs, autograd.Variable(torch.LongTensor(target)))
-			loss2 = loss_function(clas_log_probs, autograd.Variable(torch.LongTensor(target_class)))
-			loss = loss1 + loss2
-			loss.backward()
+def train_node2vec(EMBEDDING_SIZE, CONTEXT_SIZE, NODE_SIZE, Random_path, node_map_type):
+    # losses = []
+    loss_function = nn.NLLLoss()
+    model = Node2Vec(NODE_SIZE, CONTEXT_SIZE, EMBEDDING_SIZE)
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    for epoch in range(10):
+        total_loss = torch.Tensor([0])
+        for context, target in Random_path:
+            target_class = node_map_type[context[-1]]
+            context_var = autograd.Variable(torch.LongTensor(context))
+            model.zero_grad()
+            log_probs, clas_log_probs = model(context_var)
+            loss1 = loss_function(log_probs, autograd.Variable(torch.LongTensor(target)))
+            loss2 = loss_function(clas_log_probs, autograd.Variable(torch.LongTensor(target_class)))
+            loss = loss1 + loss2
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.data
+        print(total_loss)
 
 
 
 
+def get_node_map_type(G):
+    node_map_type = dict()
+    for node in G.nodes:
+        node_map_type[node] = G.nodes[node][type]
+    return node_map_type
 
+if __name__ == '__main__':
+    nx_G = read_graph()
+    G = node2vec.Graph(nx_G, is_directed=False, p=1, q=1)
+    G.preprocess_transition_probs()
+    num_walks = 100000
+    walk_length = 1000
+    walks = G.simulate_walks(num_walks, walk_length)
+    node_map_type = get_node_map_type(G)
+    EMBEDDING_SIZE = 300
+    CONTEXT_SIZE = 4
+    NODE_SIZE = len(G.nodes)
+    train_node2vec(EMBEDDING_SIZE, CONTEXT_SIZE, NODE_SIZE, walks, node_map_type)
 
+# def main(args):
+#     '''
+#     Pipeline for representational learning for all nodes in a graph.
+#     '''
+#     nx_G = read_graph()
+#     G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
+#     G.preprocess_transition_probs()
+#     walks = G.simulate_walks(args.num_walks, args.walk_length)
+# 	node_map_type = get_node_map_type(G)
+#     # learn_embeddings(walks)
+#     EMBEDDING_SIZE = 300
+#     CONTEXT_SIZE = 4
+#     NODE_SIZE = len(nx_G.nodes)
+#     train_node2vec(EMBEDDING_SIZE, CONTEXT_SIZE, NODE_SIZE, walks, node_map_type)
 
-def main(args):
-	'''
-	Pipeline for representational learning for all nodes in a graph.
-	'''
-	nx_G = read_graph()
-	G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
-	G.preprocess_transition_probs()
-	walks = G.simulate_walks(args.num_walks, args.walk_length)
-	learn_embeddings(walks)
-
-if __name__ == "__main__":
-	args = parse_args()
-	main(args)
+# if __name__ == "__main__":
+#     args = parse_args()
+#     main(args)
--- node2vec.py
+++ node2vec.py
@@ -4,216 +4,216 @@ import random
 import torch
 
 class Graph():
-	def __init__(self, nx_G, is_directed, p, q):
-		self.G = nx_G
-		self.is_directed = is_directed
-		self.p = p
-		self.q = q
+    def __init__(self, nx_G, is_directed, p, q):
+        self.G = nx_G
+        self.is_directed = is_directed
+        self.p = p
+        self.q = q
 
-	def node2vec_walk(self, walk_length, start_node):
-		'''
-		Simulate a random walk starting from start node.
-		'''
-		G = self.G
-		alias_nodes = self.alias_nodes
-		alias_edges = self.alias_edges
+    def node2vec_walk(self, walk_length, start_node):
+        '''
+        Simulate a random walk starting from start node.
+        '''
+        G = self.G
+        alias_nodes = self.alias_nodes
+        alias_edges = self.alias_edges
 
-		walk = [start_node]
+        walk = [start_node]
 
-		while len(walk) < walk_length:
-			cur = walk[-1]
-			cur_nbrs = sorted(G.neighbors(cur))
-			if len(cur_nbrs) > 0:
-				if len(walk) == 1:
-					walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
-				else:
-					prev = walk[-2]
-					next = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0], 
-						alias_edges[(prev, cur)][1])]
-					walk.append(next)
-			else:
-				break
+        while len(walk) < walk_length:
+            cur = walk[-1]
+            cur_nbrs = sorted(G.neighbors(cur))
+            if len(cur_nbrs) > 0:
+                if len(walk) == 1:
+                    walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
+                else:
+                    prev = walk[-2]
+                    next = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0],
+                        alias_edges[(prev, cur)][1])]
+                    walk.append(next)
+            else:
+                break
 
-		return walk
+        return walk
 
-	def node2vec_hin_walk(self, path, type, start_node, type_weight):
-		'''
-		Simulate a random walk in HIN from start node according to path
-		:param path: meta path such as 'APA' or 'APCPA'
-		:param type: the type name of the node. node[type] = 'A'
-		:param start_node:
-		:param type_weight: the type's weight name of the node. node[type_weight] = 1.0
-		:return: a walk path
-		'''
-		G = self.G
-		if G.nodes[start_node][type] != path[0]:
-			print 'the start node s type is not consistent with path0 type.'
-			return False
-		walk = [start_node]
+    def node2vec_hin_walk(self, path, type, start_node, type_weight):
+        '''
+        Simulate a random walk in HIN from start node according to path
+        :param path: meta path such as 'APA' or 'APCPA'
+        :param type: the type name of the node. node[type] = 'A'
+        :param start_node:
+        :param type_weight: the type's weight name of the node. node[type_weight] = 1.0
+        :return: a walk path
+        '''
+        G = self.G
+        if G.nodes[start_node][type] != path[0]:
+            print 'the start node s type is not consistent with path0 type.'
+            return False
+        walk = [start_node]
 
-		walk_length = len(path)
-		walk_step = 1
-		while len(walk) < walk_length:
-			cur = walk[-1]
-			cur_nbrs = []
-			cur_nbrs_weight = []
-			# print cur
-			for nbr_node in sorted(G.neighbors(cur)):
-				if G.nodes[nbr_node][type] == path[walk_step]:
-					cur_nbrs.append(nbr_node)
-					cur_nbrs_weight.append(G.nodes[nbr_node][type_weight])
-			if len(cur_nbrs) == 0:
-				print "can't find the type path"
-				return False
-			# print cur_nbrs
-			# print cur_nbrs_weight
-			next_node = np.random.choice(cur_nbrs, 1, replace = False, p = cur_nbrs_weight)[0]
-			walk.append(next_node)
+        walk_length = len(path)
+        walk_step = 1
+        while len(walk) < walk_length:
+            cur = walk[-1]
+            cur_nbrs = []
+            cur_nbrs_weight = []
+            # print cur
+            for nbr_node in sorted(G.neighbors(cur)):
+                if G.nodes[nbr_node][type] == path[walk_step]:
+                    cur_nbrs.append(nbr_node)
+                    cur_nbrs_weight.append(G.nodes[nbr_node][type_weight])
+            if len(cur_nbrs) == 0:
+                print "can't find the type path"
+                return False
+            # print cur_nbrs
+            # print cur_nbrs_weight
+            next_node = np.random.choice(cur_nbrs, 1, replace = False, p = cur_nbrs_weight)[0]
+            walk.append(next_node)
 
-		return walk
-
-
+        return walk
 
 
 
-	def simulate_walks(self, num_walks, walk_length):
-		'''
-		Repeatedly simulate random walks from each node.
-		'''
-		G = self.G
-		walks = []
-		nodes = list(G.nodes())
-		print 'Walk iteration:'
-		for walk_iter in range(num_walks):
-			print str(walk_iter+1), '/', str(num_walks)
-			random.shuffle(nodes)
-			for node in nodes:
-				walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
-
-		return walks
-
-	def simulate_hin_walks(self, num_walks, path, type, type_weight):
-		'''
-		Repeatedly simulate random walks from each node in HIN according to path.
-		:param num_walks: num of walks
-		:param path: meta path such as 'APA' or 'APCPA'
-		:param type: the type name of the node. node[type] = 'A'
-		:param type_weight: the type's weight name of the node. node[type_weight] = 1.0
-		:return: a list of walk
-		'''
-		G = self.G
-		walks = []
-		start_nodes = []
-		for s_node in list(G.nodes()):
-			if G.nodes[s_node][type] == path[0]:
-				start_nodes.append(s_node)
-		if len(start_nodes) == 0:
-			print 'can not find this type node'
-			return False
-		print 'Walk iteration:'
-		for walk_iter in range(num_walks):
-			print str(walk_iter+1), '/' , str(num_walks)
-			random.shuffle(start_nodes)
-			for start_node in start_nodes:
-				walk = self.node2vec_hin_walk(path, type, start_node, type_weight)
-				if walk != False:
-					walks.append(walk)
-		return walks
 
 
+    def simulate_walks(self, num_walks, walk_length):
+        '''
+        Repeatedly simulate random walks from each node.
+        '''
+        G = self.G
+        walks = []
+        nodes = list(G.nodes())
+        print 'Walk iteration:'
+        for walk_iter in range(num_walks):
+            print str(walk_iter+1), '/', str(num_walks)
+            random.shuffle(nodes)
+            for node in nodes:
+                walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
 
-	def get_alias_edge(self, src, dst):
-		'''
-		Get the alias edge setup lists for a given edge.
-		'''
-		G = self.G
-		p = self.p
-		q = self.q
+        return walks
 
-		unnormalized_probs = []
-		for dst_nbr in sorted(G.neighbors(dst)):
-			if dst_nbr == src:
-				unnormalized_probs.append(G[dst][dst_nbr]['weight']/p)
-			elif G.has_edge(dst_nbr, src):
-				unnormalized_probs.append(G[dst][dst_nbr]['weight'])
-			else:
-				unnormalized_probs.append(G[dst][dst_nbr]['weight']/q)
-		norm_const = sum(unnormalized_probs)
-		normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+    def simulate_hin_walks(self, num_walks, path, type, type_weight):
+        '''
+        Repeatedly simulate random walks from each node in HIN according to path.
+        :param num_walks: num of walks
+        :param path: meta path such as 'APA' or 'APCPA'
+        :param type: the type name of the node. node[type] = 'A'
+        :param type_weight: the type's weight name of the node. node[type_weight] = 1.0
+        :return: a list of walk
+        '''
+        G = self.G
+        walks = []
+        start_nodes = []
+        for s_node in list(G.nodes()):
+            if G.nodes[s_node][type] == path[0]:
+                start_nodes.append(s_node)
+        if len(start_nodes) == 0:
+            print 'can not find this type node'
+            return False
+        print 'Walk iteration:'
+        for walk_iter in range(num_walks):
+            print str(walk_iter+1), '/' , str(num_walks)
+            random.shuffle(start_nodes)
+            for start_node in start_nodes:
+                walk = self.node2vec_hin_walk(path, type, start_node, type_weight)
+                if walk != False:
+                    walks.append(walk)
+        return walks
 
-		return alias_setup(normalized_probs)
 
-	def preprocess_transition_probs(self):
-		'''
-		Preprocessing of transition probabilities for guiding the random walks.
-		'''
-		G = self.G
-		is_directed = self.is_directed
 
-		alias_nodes = {}
-		for node in G.nodes():
-			unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
-			norm_const = sum(unnormalized_probs)
-			normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
-			alias_nodes[node] = alias_setup(normalized_probs)
+    def get_alias_edge(self, src, dst):
+        '''
+        Get the alias edge setup lists for a given edge.
+        '''
+        G = self.G
+        p = self.p
+        q = self.q
 
-		alias_edges = {}
-		triads = {}
+        unnormalized_probs = []
+        for dst_nbr in sorted(G.neighbors(dst)):
+            if dst_nbr == src:
+                unnormalized_probs.append(G[dst][dst_nbr]['weight']/p)
+            elif G.has_edge(dst_nbr, src):
+                unnormalized_probs.append(G[dst][dst_nbr]['weight'])
+            else:
+                unnormalized_probs.append(G[dst][dst_nbr]['weight']/q)
+        norm_const = sum(unnormalized_probs)
+        normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
 
-		if is_directed:
-			for edge in G.edges():
-				alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-		else:
-			for edge in G.edges():
-				alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-				alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
+        return alias_setup(normalized_probs)
 
-		self.alias_nodes = alias_nodes
-		self.alias_edges = alias_edges
+    def preprocess_transition_probs(self):
+        '''
+        Preprocessing of transition probabilities for guiding the random walks.
+        '''
+        G = self.G
+        is_directed = self.is_directed
 
-		return
+        alias_nodes = {}
+        for node in G.nodes():
+            unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
+            norm_const = sum(unnormalized_probs)
+            normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+            alias_nodes[node] = alias_setup(normalized_probs)
+
+        alias_edges = {}
+        triads = {}
+
+        if is_directed:
+            for edge in G.edges():
+                alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
+        else:
+            for edge in G.edges():
+                alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
+                alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
+
+        self.alias_nodes = alias_nodes
+        self.alias_edges = alias_edges
+
+        return
 
 
 def alias_setup(probs):
-	'''
-	Compute utility lists for non-uniform sampling from discrete distributions.
-	Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
-	for details
-	'''
-	K = len(probs)
-	q = np.zeros(K)
-	J = np.zeros(K, dtype=np.int)
+    '''
+    Compute utility lists for non-uniform sampling from discrete distributions.
+    Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
+    for details
+    '''
+    K = len(probs)
+    q = np.zeros(K)
+    J = np.zeros(K, dtype=np.int)
 
-	smaller = []
-	larger = []
-	for kk, prob in enumerate(probs):
-	    q[kk] = K*prob
-	    if q[kk] < 1.0:
-	        smaller.append(kk)
-	    else:
-	        larger.append(kk)
+    smaller = []
+    larger = []
+    for kk, prob in enumerate(probs):
+        q[kk] = K*prob
+        if q[kk] < 1.0:
+            smaller.append(kk)
+        else:
+            larger.append(kk)
 
-	while len(smaller) > 0 and len(larger) > 0:
-	    small = smaller.pop()
-	    large = larger.pop()
+    while len(smaller) > 0 and len(larger) > 0:
+        small = smaller.pop()
+        large = larger.pop()
 
-	    J[small] = large
-	    q[large] = q[large] + q[small] - 1.0
-	    if q[large] < 1.0:
-	        smaller.append(large)
-	    else:
-	        larger.append(large)
+        J[small] = large
+        q[large] = q[large] + q[small] - 1.0
+        if q[large] < 1.0:
+            smaller.append(large)
+        else:
+            larger.append(large)
 
-	return J, q
+    return J, q
 
 def alias_draw(J, q):
-	'''
-	Draw sample from a non-uniform discrete distribution using alias sampling.
-	'''
-	K = len(J)
+    '''
+    Draw sample from a non-uniform discrete distribution using alias sampling.
+    '''
+    K = len(J)
 
-	kk = int(np.floor(np.random.rand()*K))
-	if np.random.rand() < q[kk]:
-	    return kk
-	else:
-	    return J[kk]
+    kk = int(np.floor(np.random.rand()*K))
+    if np.random.rand() < q[kk]:
+        return kk
+    else:
+        return J[kk]
